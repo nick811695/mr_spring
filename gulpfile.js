d7a84ffebe5d0d8a80eb46ea8ec1d17ffc1f4b99
yuuki5831 <roy84214@gmail.com>
2019-04-15 00:46:15 +0800

    訂房頁修改(Ver.1)

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('default', ['sass'], function () {
         server: {
             //根目錄
             baseDir: "./",
-            index: "index.html"
+            index: "reservation.html"
         }
     });
 
--- js/calendar.js
+++ js/calendar.js
@@ -143,6 +143,8 @@ window.addEventListener("load", () => {
             var num = arrmm.indexOf(document.querySelector("#mm-sp").innerText)+1;
             if (document.querySelector("#yy-sp").innerText < yyyy   ) {
                 ss.style.color="#ADADAD";
+                ss.style.backgroundColor="rgba(173, 173, 173, .5)";
+                ss.style.cursor="default";
                 // ss.style.cursor = "not-allowed";
                 ss.removeEventListener("click", tdclass);
             }
@@ -151,6 +153,8 @@ window.addEventListener("load", () => {
             if( num<mm && document.querySelector("#yy-sp").innerText == yyyy ){
                 ss.removeEventListener("click", tdclass);
                 ss.style.color="#ADADAD";
+                ss.style.backgroundColor="rgba(173, 173, 173, .5)";
+                ss.style.cursor="default";
                 //不等於今日
                 if(ss.innerText ==  today && num == mm && document.querySelector("#yy-sp").innerText == yyyy ){
                     ss.style.color="#F27F22";
@@ -161,6 +165,8 @@ window.addEventListener("load", () => {
             //同年同月 但小於今天
             if(ss.innerText<= today &&  num==mm && document.querySelector("#yy-sp").innerText == yyyy){
                 ss.style.color="#ADADAD";
+                ss.style.backgroundColor="rgba(173, 173, 173, .5)";
+                ss.style.cursor="default";
                 // ss.style.cursor = "not-allowed";
                 ss.removeEventListener("click", tdclass);
             }
