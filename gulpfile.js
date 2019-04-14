1597c33395ea9fb7c8245b3930deb48f69071778
yuuki5831 <roy84214@gmail.com>
2019-04-15 00:54:32 +0800

    自己的gulpfile.js裏頭的根目錄要記得改成準備要製作或修改的網頁 ex. index: "reservation.html"

--- gulpfile.js
+++ gulpfile.js
@@ -25,7 +25,7 @@ gulp.task('default', ['sass'], function () {
         server: {
             //根目錄
             baseDir: "./",
-            index: "reservation.html"
+            index: "index.html"
         }
     });
 
