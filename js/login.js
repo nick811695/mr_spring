88e5ed6f3a8f85427626d2eff7d886715cb5b627
mountainchang65485 <mountain0126@gmail.com>
2019-04-23 04:13:36 +0800

    header : add login light box (un rwd) + burger and menu light box (rwd) + new logo + login.js index.html : add a div to wrap second_page

---
+++ js/login.js
@@ -0,0 +1,59 @@
+function showLogin() {
+        document.getElementById("lightBox").style.display = "block";
+    // if (document.getElementById("spanLogin").innerHTML == "登入") {
+    //     document.getElementById("lightBox").style.display = "block";
+    // }
+    // else {
+    //     document.getElementById('spanLogin').innerHTML = "登入";
+    //     document.getElementById('memName').innerHTML = "";
+    //     document.getElementById('memId').value = "";
+    //     document.getElementById('memPsw').value = "";
+    // }
+}
+
+function loginForm() {
+    var account = document.getElementById('memId').value;
+    var password = document.getElementById('memPsw').value;
+
+    if (document.getElementById('memId').value == "mountain" && document.getElementById('memPsw').value == "123456") {
+
+        // document.getElementById('spanLogin').innerHTML = "登出";
+        document.getElementById('memName').innerHTML = "張君山";
+        document.getElementById("memId").value = "";
+        document.getElementById("memPsw").value = "";
+        document.getElementById("lightBox").style.display = "none";
+    }
+    else {
+        alert("帳號或密碼錯誤，請重新輸入");
+    }
+}
+
+function logOut(){
+      if( document.getElementById('spanLogin').innerHTML == "登出"){
+
+      }
+}
+
+function cancelLogin() {
+    document.getElementById("memId").value = "";
+    document.getElementById("memPsw").value = "";
+    document.getElementById("lightBox").style.display = "none";
+}
+
+function init() {
+    var btnLogin = document.getElementById("spanLogin");
+    btnLogin.addEventListener("click", showLogin, false);
+
+    var btnLogOut = document.getElementById("spanLogin");
+    btnLogin.addEventListener("click", logOut, false);
+
+    var btnSendForm = document.getElementById("btnLogin");
+    btnSendForm.addEventListener("click", loginForm, false);
+
+
+    var btnLogCancel = document.getElementById("btnLoginCancel");
+    btnLogCancel.addEventListener("click", cancelLogin, false);
+
+};
+
+window.onload = init;
