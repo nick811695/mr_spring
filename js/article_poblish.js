365e948a1a8afb0fd90988b911fa7aae83422643
hsu.jieh <hsu.jieh@gmail.com>
2019-04-24 03:27:38 +0800

    no message

--- js/article_poblish.js
+++ js/article_poblish.js
@@ -1,50 +1,42 @@
 $(document).ready(function(){
 	window_width = $(window).width();
-	$(".lightbox_filter").click(function(){
-		if ($(".lightbox_filter_box").hasClass("open") == false) {
-			$(".lightbox_filter_box").addClass("open");
-			$('#lightbox_filter_mask').css("display","block");
-		}else{
-			$(".lightbox_filter_box").removeClass("open");
-			$('#lightbox_filter_mask').css("display","none");
-		}
-	});
-	$('#lightbox_filter_mask').click(function(){
-		$(".lightbox_filter_box").removeClass("open");
-		$('#lightbox_filter_mask').css("display","none");
-	});
-	
+
 });
 
 
 var lightboxOnBtn = document.getElementsByClassName("springCard");
-var lightboxOffBtn = document.getElementById("lightboxToggle");
+// var lightboxOffBtn = document.getElementById("lightboxToggle");
 
 
 lightboxOnBtn[0].addEventListener("click",lightboxOn);
-lightboxOffBtn.addEventListener("click",lightboxOff);
+// lightboxOffBtn.addEventListener("click",lightboxOff);
 document.getElementById('add_card').addEventListener("click",lightboxOn);
 function lightboxOn(){
     document.getElementById('lightbox_wrapper').style.visibility = "visible";
+    $('body').css({
+    	"height":"100vh",
+    });
     $('.card').click(function(){
     	$(".card").removeClass("card_selected");
     	$(this).addClass("card_selected");
-
+      $('#nextStep').click(function(){
+        $("#lightbox_wrapper").css("visibility","hidden");
+      });
 		temp_card = $(this).parent('.cards').html();
 		// card_name = $(this).children(".card_name").text();
 		// card_item1 = $(this).children(".card_item_box").children(".card_item1").children(".card_item_name").html();
 		// card_item2 = $(this).children(".card_item_box").children(".card_item2").children(".card_item_name").html();
 		// card_item3 = $(this).children(".card_item_box").children(".card_item3").children(".card_item_name").html();
 		// card_item4 = $(this).children(".card_item_box").children(".card_item4").children(".card_item_name").html();
-		if (window_width<=768) {
+		if (window_width<=1200) {
 			$("#lightbox_radar_btn").css("display","block");
 			$("#lightbox_radar_btn").click(function(){
 				$("#lightbox_radar").css("display","block");
-				$("#lightbox_radar_mask").css("display","block");
+				// $("#lightbox_radar_mask").css("display","block");
 			});
 			$("#lightbox_radar_close").click(function(){
 				$("#lightbox_radar").css("display","none");
-				$("#lightbox_radar_mask").css("display","none");
+				// $("#lightbox_radar_mask").css("display","none");
 			});
 		}
 		
@@ -53,13 +45,17 @@ function lightboxOn(){
 			$('#add_card').html(temp_card);
 			$('.springCard').css('display','none');
 			// $('.selected_card_data').html("<h3>"+card_name+"</h3>"+"<div class='item_box'>"+card_item1+card_item2+card_item3+card_item4+"</div>");
-			
+			$('body').css({
+		    	"height":"auto",
+		    });
 			if (window_width<=768) {
 				$('.selected_card_data').html(temp_card);
 				$('.selected_card_data').addClass('change_card');
 				$('.selected_card_data').css('margin-bottom','0px');
 				$('.forum_wrap .article_publish').css('padding-top','10px');
 				$('.change_card').click(lightboxOn);
+				$("#lightbox_radar").css("display","none");
+
 			}
 			
 			
@@ -69,26 +65,10 @@ function lightboxOn(){
 	});
 }
 
-function lightboxOff(){
-    document.getElementById('lightbox_wrapper').style.visibility = "hidden";
-
-}
 
 
 
 
-/*----------*/
-$(document).ready(function(){
-  /*點確定關閉燈箱*/
-  $(".card").click(function(){
-    $('#nextStep').click(function(){
-      $("#lightbox_wrapper").css("visibility","hidden");
-    });
-    /*選到的卡給class*/
-    $(".card").removeClass("card_selected");
-    $(this).addClass("card_selected");
-  });
-});
 
 $("select").on("click" , function() {
   
